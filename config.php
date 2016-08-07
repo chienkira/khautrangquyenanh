a08cfea34b310969c38d0f04cc198b5d37ee1d95
Kira OnSrv <chienhedspi@gmail.com>
2016-08-07 12:18:57 +0900

    cai dat co ban

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://happydev.xyz/khautrang/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://happydev.xyz/khautrang/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/khautrang/catalog/');
+define('DIR_SYSTEM', '/var/www/html/khautrang/system/');
+define('DIR_LANGUAGE', '/var/www/html/khautrang/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/khautrang/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/khautrang/system/config/');
+define('DIR_IMAGE', '/var/www/html/khautrang/image/');
+define('DIR_CACHE', '/var/www/html/khautrang/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/khautrang/system/download/');
+define('DIR_UPLOAD', '/var/www/html/khautrang/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/khautrang/system/modification/');
+define('DIR_LOGS', '/var/www/html/khautrang/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'khautrang');
+define('DB_PASSWORD', 'P@ssW0rdKh@uTr@ng');
+define('DB_DATABASE', 'khautrang');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
