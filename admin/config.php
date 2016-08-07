bc55d038084c132439a1b9d30a83f8d11c924daf
ChienKira <chienhedspi@gmail.com>
2016-08-07 18:43:49 +0900

    update config

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://happydev.xyz/khautrang/admin/');
-define('HTTP_CATALOG', 'http://happydev.xyz/khautrang/');
+define('HTTP_SERVER', 'http://happydev.xyz/khautrangquyenanh/admin/');
+define('HTTP_CATALOG', 'http://happydev.xyz/khautrangquyenanh/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://happydev.xyz/khautrang/admin/');
-define('HTTPS_CATALOG', 'http://happydev.xyz/khautrang/');
+define('HTTPS_SERVER', 'http://happydev.xyz/khautrangquyenanh/admin/');
+define('HTTPS_CATALOG', 'http://happydev.xyz/khautrangquyenanh/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/khautrang/admin/');
-define('DIR_SYSTEM', '/var/www/html/khautrang/system/');
-define('DIR_LANGUAGE', '/var/www/html/khautrang/admin/language/');
-define('DIR_TEMPLATE', '/var/www/html/khautrang/admin/view/template/');
-define('DIR_CONFIG', '/var/www/html/khautrang/system/config/');
-define('DIR_IMAGE', '/var/www/html/khautrang/image/');
-define('DIR_CACHE', '/var/www/html/khautrang/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/khautrang/system/download/');
-define('DIR_UPLOAD', '/var/www/html/khautrang/system/upload/');
-define('DIR_LOGS', '/var/www/html/khautrang/system/logs/');
-define('DIR_MODIFICATION', '/var/www/html/khautrang/system/modification/');
-define('DIR_CATALOG', '/var/www/html/khautrang/catalog/');
+define('DIR_APPLICATION', '/var/www/html/khautrangquyenanh/admin/');
+define('DIR_SYSTEM', '/var/www/html/khautrangquyenanh/system/');
+define('DIR_LANGUAGE', '/var/www/html/khautrangquyenanh/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/khautrangquyenanh/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/khautrangquyenanh/system/config/');
+define('DIR_IMAGE', '/var/www/html/khautrangquyenanh/image/');
+define('DIR_CACHE', '/var/www/html/khautrangquyenanh/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/khautrangquyenanh/system/download/');
+define('DIR_UPLOAD', '/var/www/html/khautrangquyenanh/system/upload/');
+define('DIR_LOGS', '/var/www/html/khautrangquyenanh/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/khautrangquyenanh/system/modification/');
+define('DIR_CATALOG', '/var/www/html/khautrangquyenanh/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'khautrang');
 define('DB_PASSWORD', 'P@ssW0rdKh@uTr@ng');
-define('DB_DATABASE', 'khautrang');
+define('DB_DATABASE', 'khautrangquyenanh');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
